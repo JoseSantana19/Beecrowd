7ef64bbc69052ac1d0d2080c01c87f437a7c4e13
José Luís Fernando do Carmo Santana <jlfernando2001@gmail.com>
2026-03-01 09:16:36 -0300

    Create BEE_2742.sql

---
+++ SQL/BEE_2742.sql
@@ -0,0 +1,8 @@
+SELECT
+    lr.name,
+    ROUND(lr.omega * 1.618, 3) AS "Fator N"
+FROM life_registry lr
+INNER JOIN dimensions d
+    ON lr.dimensions_id = d.id
+WHERE d.name IN ('C875','C774')
+    AND lr.name LIKE 'Richard%'
