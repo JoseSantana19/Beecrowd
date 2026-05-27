5340fb1614c8a58924079196fb77be0836761d5a
José Luís Fernando do Carmo Santana <jlfernando2001@gmail.com>
2026-05-26 21:24:05 -0300

    Create BEE_1050.py

---
+++ PYTHON/BEE_1050.py
@@ -0,0 +1,22 @@
+ddd = input()
+
+if ddd == '61':
+    dest = 'Brasilia'
+elif ddd == '71':
+    dest = 'Salvador'
+elif ddd == '11':
+    dest = 'Sao Paulo'
+elif ddd == '21':
+    dest = 'Rio de Janeiro'
+elif ddd == '32':
+    dest = 'Juiz de Fora'
+elif ddd == '19':
+    dest = 'Campinas'
+elif ddd == '27':
+    dest = 'Vitoria'
+elif ddd == '31':
+    dest = 'Belo Horizonte'
+else:
+    dest = 'DDD nao cadastrado'
+
+print(dest)
