11c9f9486bf5ee4763a221ec48753e85a448c096
José Luís Fernando do Carmo Santana <jlfernando2001@gmail.com>
2026-06-03 20:32:06 -0300

    Create BEE_1052.py

---
+++ PYTHON/BEE_1052.py
@@ -0,0 +1,17 @@
+meses = [
+'January',
+'February',
+'March',
+'April',
+'May',
+'June',
+'July',
+'August',
+'September',
+'October',
+'November',
+'December'
+]
+
+entrada = int(input())
+print(meses[entrada-1])
