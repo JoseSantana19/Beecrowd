1b994b29c01b174f8bf183944cfa5b4bee20976e
José Luís Fernando do Carmo Santana <jlfernando2001@gmail.com>
2026-06-14 20:59:50 -0300

    Create BEE_1074.py

---
+++ PYTHON/BEE_1074.py
@@ -0,0 +1,16 @@
+n = int(input())
+
+for _ in range(n):
+    
+    x = int(input())
+    
+    if x > 0 and x % 2 == 0:
+        print('EVEN POSITIVE')
+    elif x > 0 and x % 2 == 1:
+        print('ODD POSITIVE')
+    elif x < 0 and x % 2 == 0:
+        print('EVEN NEGATIVE')
+    elif x < 0 and x % 2 == 1:
+        print('ODD NEGATIVE')
+    else:
+        print('NULL')
