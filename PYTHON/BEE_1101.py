bd5653cd6fedec761c890929153c99e5742be559
José Luís Fernando do Carmo Santana <jlfernando2001@gmail.com>
2026-06-27 13:31:51 -0300

    Add BEE_1101.py for summing integers in range

---
+++ PYTHON/BEE_1101.py
@@ -0,0 +1,17 @@
+while True:
+    m, n = list(map(int, input().split()))
+    
+    if m <= 0 or n <= 0:
+        break
+        
+    if m > n:
+        m, n = n, m
+        
+    saida = ''
+    soma = 0
+    for e in range(m,n+1):
+        saida += f"{e} "
+        soma += e
+    saida += f"Sum={soma}"
+    
+    print(saida)
