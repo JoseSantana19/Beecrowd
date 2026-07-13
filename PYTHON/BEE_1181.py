cefb68c1c00252de3f700c71ebde030d90aac994
José Luís Fernando do Carmo Santana <jlfernando2001@gmail.com>
2026-07-13 19:56:57 -0300

    Create BEE_1181.py

---
+++ PYTHON/BEE_1181.py
@@ -0,0 +1,12 @@
+l = int(input())
+operacao = input()
+soma = 0
+for linha in range(12):
+    for coluna in range(12):
+        valor = float(input())
+        if linha == l:
+            soma += valor
+if operacao == 'S':
+    print(f"{soma:.1f}")
+elif operacao == 'M':
+    print(f"{soma/12:.1f}")
