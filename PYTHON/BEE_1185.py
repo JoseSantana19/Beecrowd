604f7698c153a8b00ec926cba9ff6c273d1d4241
José Luís Fernando do Carmo Santana <jlfernando2001@gmail.com>
2026-07-14 20:35:15 -0300

    Create BEE_1185.py

---
+++ PYTHON/BEE_1185.py
@@ -0,0 +1,13 @@
+operacao = input()
+soma = 0
+cont = 0
+for linha in range(12):
+    for coluna in range(12):
+        valor = float(input())
+        if linha + coluna <= 10:
+            soma += valor
+            cont += 1
+if operacao == 'S':
+    print(f"{soma:.1f}")
+elif operacao == 'M':
+    print(f"{soma/cont:.1f}")
