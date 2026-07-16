ddb2a68c8a7c7f30a47befe340119d657c5857e5
José Luís Fernando do Carmo Santana <jlfernando2001@gmail.com>
2026-07-16 19:09:01 -0300

    Create BEE_1188.py

---
+++ PYTHON/BEE_1188.py
@@ -0,0 +1,17 @@
+operacao = input()
+soma = 0
+cont = 0
+col_min, col_max = 5, 6
+for linha in range(12):
+    for coluna in range(12):
+        valor = float(input())
+        if linha > 6 and col_min <= coluna and col_max >= coluna:
+            soma += valor
+            cont += 1
+    if linha > 6:
+        col_min -= 1
+        col_max += 1
+if operacao == 'S':
+    print(f"{soma:.1f}")
+elif operacao == 'M':
+    print(f"{soma/cont:.1f}")
